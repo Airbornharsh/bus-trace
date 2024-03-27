025c83befeda2193061b5596b71b13cb90d12d32
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-03-27 21:14:24 +0530

    Updated the Get Uid from Token

--- http-server/pkg/helpers/auth.go
+++ http-server/pkg/helpers/auth.go
@@ -2,13 +2,22 @@ package helpers
 
 import (
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
 
 func TokenToUid(c *gin.Context) (int, error, string) {
-	tokenString := c.Request.Header.Get("Authorization")
+	authorization := c.Request.Header.Get("Authorization")
+	if authorization == "" {
+		return 401, nil, ""
+	}
+	auth2 := strings.Split(authorization, " ")
+	if len(auth2) < 2 {
+		return 401, nil, ""
+	}
+	tokenString := auth2[1]
 	jwtSecret := os.Getenv("JWT_SECRET")
 	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
 		return []byte(jwtSecret), nil
