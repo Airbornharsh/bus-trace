539deda003171dae18eb1a5263404bdd38ffeb15
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-03-29 00:19:28 +0530

    Updatd

--- http-server/cmd/main.go
+++ http-server/cmd/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/airbornharsh/bus-trace/http-server/internal/db"
 	"github.com/airbornharsh/bus-trace/http-server/pkg/routes"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -21,10 +20,7 @@ func main() {
 		fmt.Println("Error in Loading Env File")
 	}
 
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"*"}
-	config.AllowHeaders = []string{"*"}
-	r.Use(cors.New(config))
+	r.Use(CorsMiddleware())
 
 	db.DbInit()
 
@@ -32,9 +28,35 @@ func main() {
 		c.Status(http.StatusOK)
 	})
 
+	r.POST("/", func(c *gin.Context) {
+		fmt.Println(c)
+		fmt.Println("Hello World POST")
+		c.JSON(200, gin.H{
+			"message": "Welcome to Bus Trace Http Backend POST",
+		})
+	})
+
 	routes.RouteInit(r)
 
 	fmt.Println("Routes")
 
 	r.Run(":8001")
+}
+
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Allow requests from any origin with the specified methods
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight requests
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		// Continue with the next middleware/handler
+		c.Next()
+	}
 }
