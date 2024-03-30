a244209146147d5c6097c8c6eb4e23130f60d443
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-03-30 20:01:23 +0530

    Mutex

---
+++ websocket-server/pkg/helpers/mutex/init.go
@@ -0,0 +1,8 @@
+package mutex
+
+var BusClientsMutex bool = true
+var ClientsMutex bool = true
+var BusOwnerMutex bool = true
+var ClientLocationMutex bool = true
+var BusLocationMutex bool = true
+var UserClientMutex bool = true
